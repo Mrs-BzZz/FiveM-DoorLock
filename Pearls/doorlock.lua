2e239daffded847541fd26bf4e5113ea8518507b
BzZz <adelheid.kaszubi@gmail.com>
2020-04-25 18:01:39 +0200

    Create doorlock.lua

---
+++ Pearls/doorlock.lua
@@ -0,0 +1,73 @@
+--	
+-- Pearls
+--
+	{
+		textCoords = vector3(-1816.95, -1194.26, 14.6),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'sibaaa_pearls_maindoor',
+				objYaw = 60.0,
+				objCoords = vector3(-1817.901, -1193.668, 14.8632)
+			},
+
+			{
+				objName = 'sibaaa_pearls_maindoor',
+				objYaw = -120.0,
+				objCoords = vector3(-1815.884, -1194.833, 14.8632)
+			}
+		}
+	},
+	{
+		objName = 'sibaaa_pearls_frontdoor',
+		objYaw = 60.0,
+		objCoords  = vector3(-1831.575, -1180.974, 14.7496),
+		textCoords = vector3(-1831.05, -1181.4, 14.6),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},	
+	{
+		objName = 'sibaaa_pearls_kitchendoor',
+		objYaw = -30.0,
+		objCoords  = vector3(-1847.270, -1189.992, 14.4712),
+		textCoords = vector3(-1846.64, -1190.51, 14.6),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	{
+		objName = 'sibaaa_pearls_frontdoor',
+		objYaw = 60.0,
+		objCoords  = vector3(-1840.114, -1181.331, 14.7444),
+		textCoords = vector3(-1839.43, -1181.65, 14.6),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	{
+		textCoords = vector3(-1842.47, -1199.03, 14.6),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'sibaaa_pearls_maindoor',
+				objYaw = -30.0,
+				objCoords = vector3(-1841.884, -1198.019, 14.8637)
+			},
+
+			{
+				objName = 'sibaaa_pearls_maindoor',
+				objYaw = 150.0,
+				objCoords = vector3(-1843.049, -1200.038, 14.8637)
+			}
+		}
+	},
