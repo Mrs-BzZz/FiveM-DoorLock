2a0617f0d22fc096f9934578532d7159afccb6fb
BzZz <adelheid.kaszubi@gmail.com>
2020-04-25 16:07:46 +0200

    Create doorlock.lua

---
+++ vinařství/doorlock.lua
@@ -0,0 +1,244 @@
+	--
+	-- Vinice
+	--
+		
+	{
+		textCoords = vector3(-1911.13, 2074.7, 141.0),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = 140.0,
+				objCoords = vector3(-1912.101, 2075.5601, 140.9149)
+			},
+
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = -40.0,
+				objCoords = vector3(-1910.209, 2073.9690, 140.9131)
+			}
+		}
+	},
+	
+	{
+		textCoords = vector3(-1908.75, 2072.61, 141.0),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = -40.0,
+				objCoords = vector3(-1907.732, 2071.8779, 140.9131)
+			},
+
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = 140.0,
+				objCoords = vector3(-1909.624, 2073.4719, 140.9152)
+			}
+		}
+	},
+	
+	{
+		textCoords = vector3(-1911.14, 2079.83, 141.0),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = -130.0,
+				objCoords = vector3(-1911.810, 2078.7949, 140.9115)
+			},
+
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = 50.0,
+				objCoords = vector3(-1910.202, 2080.6790, 140.9115)
+			}
+		}
+	},
+	
+	{
+		textCoords = vector3(-1906.94, 2084.81, 141.0),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = -130.0,
+				objCoords = vector3(-1907.596, 2083.7439, 140.9115)
+			},
+
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = 50.0,
+				objCoords = vector3(-1905.994, 2085.6270, 140.9115)
+			}
+		}
+	},
+	
+	{
+		textCoords = vector3(-1901.87, 2085.82, 141.0),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = 140.0,
+				objCoords = vector3(-1902.882, 2086.5449, 140.9168)
+			},
+
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = -40.0,
+				objCoords = vector3(-1900.993, 2084.9473, 140.9188)
+			}
+		}
+	},
+	
+	{
+		textCoords = vector3(-1899.51, 2083.73, 141),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = 140.0,
+				objCoords = vector3(-1900.406, 2084.4468, 140.9146)
+			},
+
+			{
+				objName = 'ball_prop_italy3',
+				objYaw = -40.0,
+				objCoords = vector3(-1898.514, 2082.8521, 140.9153)
+			}
+		}
+	},
+	
+	{
+		textCoords = vector3(-1893.7, 2075.33, 141.7),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = 140.0,
+				objCoords = vector3(-1894.730, 2075.9670, 141.3125)
+			},
+
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = -40.0,
+				objCoords = vector3(-1892.833, 2074.3809, 141.3085)
+			}
+		}
+	},
+	
+	{
+		textCoords = vector3(-1889.16, 2051.67, 141.7),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = -20.0,
+				objCoords = vector3(-1887.903, 2051.3870, 141.3115)
+			},
+
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = 160.0,
+				objCoords = vector3(-1890.225, 2052.2358, 141.3125)
+			}
+		}
+	},
+	
+	{
+		textCoords = vector3(-1886.04, 2073.86, 141.7),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = 160.0,
+				objCoords = vector3(-1887.243, 2074.3079, 141.3125)
+			},
+
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = -20.0,
+				objCoords = vector3(-1884.921, 2073.4619, 141.3085)
+			}
+		}
+	},
+
+	{
+		textCoords = vector3(-1874.4, 2069.62, 141.7),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = 160.0,
+				objCoords = vector3(-1875.614, 2070.0679, 141.3125)
+			},
+
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = -20.0,
+				objCoords = vector3(-1873.293, 2069.2197, 141.3085)
+			}
+		}
+	},
+	
+	{
+		textCoords = vector3(-1886.4, 2050.8, 141.7),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = -20.0,
+				objCoords = vector3(-1885.211, 2050.3799, 141.3085)
+			},
+
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = 160.0,
+				objCoords = vector3(-1887.534, 2051.2339, 141.3125)
+			}
+		}
+	},
+	
+	{
+		textCoords = vector3(-1860.41, 2054.01, 141.6),
+		authorizedJobs = { 'vigne' },
+		locked = true,
+		distance = 2.5,
+		doors = {
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = 0.0,
+				objCoords = vector3(-1859.214, 2054.1179, 141.3535)
+			},
+
+			{
+				objName = 'ball_prop_italy2',
+				objYaw = 180.0,
+				objCoords = vector3(-1861.689, 2054.1160, 141.3536)
+			}
+		}
+	},	
+}
