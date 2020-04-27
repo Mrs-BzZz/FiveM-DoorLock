b368601e7bc91f280089d96cb42bce2374cd464a
BzZz <adelheid.kaszubi@gmail.com>
2020-04-28 00:13:52 +0200

    Create doorlock.lua

---
+++ Ballas/doorlock.lua
@@ -0,0 +1,149 @@
+	-- Ballas
+	-- vchod
+	{
+		objName = 'ball_prop_sync_door03_r',
+		objYaw = 20.0,
+		objCoords  = vector3(115.0398, -1961.363, 21.4227),
+		textCoords = vector3(114.29, -1961.58, 21.7),
+		authorizedJobs = { 'ballas' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	-- garaze vrata
+	{
+		objName = 'prop_garagel_door_02',
+		objYaw = -5.0,
+		objCoords  = vector3(102.7389, -1960.259, 21.0074),
+		textCoords = vector3(102.84, -1960.1, 20.9),
+		authorizedJobs = { 'ballas' },
+		locked = true,
+		distance = 20,
+		size = 0.7
+	},
+	-- garaze dvere
+	{
+		objName = 'ball_prop_sync_door03g_r',
+		objYaw = 85.0,
+		objCoords  = vector3(105.9584, -1963.830, 20.9406),
+		textCoords = vector3(105.76, -1964.54, 21.3),
+		authorizedJobs = { 'ballas' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	-- kulna dvere
+	{
+		objName = 'ball_prop_sync_door03g_r',
+		objYaw = -158.0,
+		objCoords  = vector3(104.6254, -1977.140, 21.0614),
+		textCoords = vector3(105.21, -1976.96, 21.4),
+		authorizedJobs = { 'ballas' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	{
+		objName = 'ball_prop_sync_door03g_l',
+		objYaw = -68.0,
+		objCoords  = vector3(111.4391, -1979.291, 21.0599),
+		textCoords = vector3(111.18, -1978.84, 21.3),
+		authorizedJobs = { 'ballas' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	-- kulna vrata
+	{
+		textCoords = vector3(107.78, -1975.84, 21.4),
+		authorizedJobs = { 'ballas' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'prop_garagel_door_01m_l',
+				objYaw = 20.0,
+				objCoords = vector3(109.1362, -1975.460, 21.1678)
+			},
+
+			{
+				objName = 'prop_garagel_door_01m_r',
+				objYaw = -157.0,
+				objCoords = vector3(106.5192, -1976.445, 21.1692)
+			}
+		}
+	},
+	-- kulna vrata 1+1
+	{
+		textCoords = vector3(94.82, -1985.13, 20.9),
+		authorizedJobs = { 'ballas' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'prop_garagel_door_01_r',
+				objYaw = 135.0,
+				objCoords = vector3(93.7088, -1984.100, 20.6204)
+			},
+
+			{
+				objName = 'prop_garagel_door_01_l',
+				objYaw = -48.0,
+				objCoords = vector3(95.8308, -1986.404, 20.6204)
+			}
+		}
+	},
+	{
+		textCoords = vector3(92.16, -1982.35, 20.9),
+		authorizedJobs = { 'ballas' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'prop_garagel_door_01_l',
+				objYaw = -48.0,
+				objCoords = vector3(93.1628, -1983.576, 20.6204)
+			},
+
+			{
+				objName = 'prop_garagel_door_01_r',
+				objYaw = 135.0,
+				objCoords = vector3(91.0369, -1981.275, 20.6204)
+			}
+		}
+	},
+	-- kuchyn
+	{
+		objName = 'ball_prop_sync_door03_l',
+		objYaw = 21.0,
+		objCoords  = vector3(113.9327, -1973.500, 21.4238),
+		textCoords = vector3(113.31, -1973.76, 21.9),
+		authorizedJobs = { 'ballas' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	-- zadni dvoudvere
+	{
+		textCoords = vector3(118.19, -1973.99, 21.9),
+		authorizedJobs = { 'ballas' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'ball_prop_sync_door03m_l',
+				objYaw = -160.0,
+				objCoords = vector3(117.0994, -1974.348, 21.4193)
+			},
+
+			{
+				objName = 'ball_prop_sync_door03m_r',
+				objYaw = 21.0,
+				objCoords = vector3(119.1674, -1973.637, 21.4193)
+			}
+		}
+	},
