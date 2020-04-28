80c81d7b0246fbaf133572cfc5b44d83230b4cb2
BzZz <adelheid.kaszubi@gmail.com>
2020-04-28 21:38:12 +0200

    Create doorlock.lua

---
+++ Autocare/doorlock.lua
@@ -0,0 +1,46 @@
+	--
+	-- Addon Mechanic LSC
+	-- vchod
+	{
+		objName = 'gabz_imp_garage_door',
+		objYaw = 3.10,
+		objCoords  = vector3(945.9352, -985.5849, 41.2282),
+		textCoords = vector3(945.98, -985.91, 40.7),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 20,
+		size = 2.0
+	},
+	-- dveře do neznáma :D
+		{
+		objName = 'v_ilev_vag_door',
+		objYaw = 95.0,
+		objCoords  = vector3(911.0059, -976.2169, 39.6507),
+		textCoords = vector3(911.11, -976.91, 40.1),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 1.0
+	},
+	-- pracovna
+	{
+		objName = 'v_ilev_roc_door2',
+		objYaw = -176.0,
+		objCoords  = vector3(955.3583, -972.4452, 39.6479),
+		textCoords = vector3(954.59, -972.38, 39.9),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	-- pracovna
+	{
+		objName = 'v_ilev_roc_door3',
+		objYaw = 95.0,
+		objCoords  = vector3(948.5289, -965.3519, 39.6435),
+		textCoords = vector3(948.42, -964.69, 39.9),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
