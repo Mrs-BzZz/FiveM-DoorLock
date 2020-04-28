d3e9613eae85170fe9707b181967bdb20a9a388c
BzZz <adelheid.kaszubi@gmail.com>
2020-04-28 22:32:42 +0200

    Create doorlock.lua

---
+++ La Fuente Blanca Ranch/doorlock.lua
@@ -0,0 +1,240 @@
+	-- Ranch
+	-- vstupni
+	{
+		textCoords = vector3(1395.95, 1141.86, 115.0),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'v_ilev_ra_door4l',
+				objYaw = -90.0,
+				objCoords = vector3(1395.9203, 1142.9041, 114.7902)
+			},
+
+			{
+				objName = 'v_ilev_ra_door4r',
+				objYaw = 90.0,
+				objCoords = vector3(1395.9199, 1140.7046, 114.7902)
+			}
+		}
+	},
+	-- koupelna
+	{
+		objName = 'v_ilev_ra_door2',
+		objYaw = -90.0,
+		objCoords  = vector3(1397.9570, 1157.5695, 114.4835),
+		textCoords = vector3(1397.95, 1156.88, 114.8),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	-- zasedaci mistnost
+	{
+		objName = 'v_ilev_ra_door2',
+		objYaw = 90.0,
+		objCoords  = vector3(1400.9962, 1158.9506, 114.4835),
+		textCoords = vector3(1400.99, 1159.65, 114.8),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	-- pracovna
+	{
+		objName = 'v_ilev_ra_door2',
+		objYaw = -90.0,
+		objCoords  = vector3(1397.0624, 1164.7397, 114.4835),
+		textCoords = vector3(1396.95, 1164.05, 114.8),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	-- sklad
+	{
+		objName = 'v_ilev_ra_door2',
+		objYaw = 180.0,
+		objCoords  = vector3(1403.4775, 1157.9828, 114.4828),
+		textCoords = vector3(1402.8, 1158.03, 114.8),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	-- veranda
+	{
+		textCoords = vector3(1390.62, 1132.19, 114.7),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'v_ilev_ra_door1_r',
+				objYaw = 90.0,
+				objCoords = vector3(1390.666, 1133.317, 114.4808)
+			},
+
+			{
+				objName = 'v_ilev_ra_door1_l',
+				objYaw = 90.0,
+				objCoords = vector3(1390.666, 1131.117, 114.4808)
+			}
+		}
+	},
+	{
+		textCoords = vector3(1400.51, 1128.29, 114.7),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'v_ilev_ra_door1_r',
+				objYaw = 180.0,
+				objCoords = vector3(1399.393, 1128.314, 114.4836)
+			},
+
+			{
+				objName = 'v_ilev_ra_door1_l',
+				objYaw = 180.0,
+				objCoords = vector3(1401.59, 1128.314, 114.4836)
+			}
+		}
+	},
+	{
+		textCoords = vector3(1408.15, 1160.06, 114.7),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'v_ilev_ra_door1_r',
+				objYaw = 90.0,
+				objCoords = vector3(1408.167, 1161.155, 114.4873)
+			},
+
+			{
+				objName = 'v_ilev_ra_door1_l',
+				objYaw = 90.0,
+				objCoords = vector3(1408.157, 1158.956, 114.4873)
+			}
+		}
+	},
+	{
+		textCoords = vector3(1408.18, 1164.73, 114.7),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'v_ilev_ra_door1_r',
+				objYaw = 90.0,
+				objCoords = vector3(1408.166, 1165.834, 114.4873)
+			},
+
+			{
+				objName = 'v_ilev_ra_door1_l',
+				objYaw = 90.0,
+				objCoords = vector3(1408.171, 1163.633, 114.4873)
+			}
+		}
+	},
+	{
+		textCoords = vector3(1390.47, 1162.35, 114.7),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'v_ilev_ra_door1_r',
+				objYaw = 90.0,
+				objCoords = vector3(1390.424, 1163.438, 114.4873)
+			},
+
+			{
+				objName = 'v_ilev_ra_door1_l',
+				objYaw = 90.0,
+				objCoords = vector3(1390.411, 1161.241, 114.4873)
+			}
+		}
+	},
+	-- sklep
+	{
+		objName = 'v_ilev_ra_door4r',
+		objYaw = 180.0,
+		objCoords  = vector3(1407.5465, 1128.3287, 114.4855),
+		textCoords = vector3(1407.04, 1128.37, 114.7),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	{
+		objName = 'v_ilev_rc_door2',
+		objYaw = 0.0,
+		objCoords  = vector3(1397.7185, 1132.4718, 109.8933),
+		textCoords = vector3(1398.46, 1132.43, 110.2),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 1.2,
+		size = 0.7
+	},
+	{
+		objName = 'v_ilev_rc_door2',
+		objYaw = 90.0,
+		objCoords  = vector3(1396.6885, 1133.3308, 109.8933),
+		textCoords = vector3(1396.68, 1133.96, 110.2),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 1.2,
+		size = 0.7
+	},
+	{
+		objName = 'v_ilev_rc_door2',
+		objYaw = 90.0,
+		objCoords  = vector3(1400.0455, 1136.1288, 109.8933),
+		textCoords = vector3(1400.04, 1136.87, 110.2),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.0,
+		size = 0.7
+	},
+	{
+		objName = 'v_ilev_rc_door2',
+		objYaw = 180.0,
+		objCoords  = vector3(1399.0165, 1143.1438, 109.8933),
+		textCoords = vector3(1398.32, 1143.05, 110.2),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.7
+	},
+	-- vratka venku
+		{
+		textCoords = vector3(1428.67, 1141.85, 114.7),
+		authorizedJobs = { 'police' },
+		locked = true,
+		distance = 2.5,
+		size = 0.6,
+		doors = {
+			{
+				objName = 'prop_lrggate_05a',
+				objYaw = 90.0,
+				objCoords = vector3(1428.5814, 1143.1166, 114.3691)
+			},
+
+			{
+				objName = 'prop_lrggate_05a',
+				objYaw = -90.0,
+				objCoords = vector3(1428.581, 1140.663, 114.3691)
+			}
+		}
+	},
+	
